d01fd1b762c352d92d2f021155452b75c085b19d
Wiwok <Emil.dechenaud@gmail.com>
2023-06-01 16:11:17 +0200

    v2.4.2

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import HighLevelActions from './Functions/HighLevelActions';
 import OnlineInteractions from './Functions/OnlineInteractions';
 import UserInteration from './Functions/UserInteraction';
 
-const APPV = '2.4.1';
+const APPV = '2.4.2';
 
 async function Install() {
 	console.clear();
