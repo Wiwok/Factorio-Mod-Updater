19bd0f5466c8ae3715001064f28ea68267111042
Emile Dechenaud <emil.dechenaud@gmail.com>
2025-09-28 11:35:57 +0200

    Update to 2.6.0

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import HighLevelActions from './Functions/HighLevelActions';
 import OnlineInteractions from './Functions/OnlineInteractions';
 import UserInteration from './Functions/UserInteraction';
 
-const APPV = '2.5.0';
+const APPV = '2.6.0';
 
 async function Install() {
 	console.clear();
